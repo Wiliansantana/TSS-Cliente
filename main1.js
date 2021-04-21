b153dfd5b7868f46ab84d3f6b4aefb08dbba6606
Wilian Santana <48594322+Wiliansantana@users.noreply.github.com>
2021-04-21 20:41:23 -0300

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,78 @@
+document.getElementById('formulario').addEventListener('submit', cadastraVeiculo);
+
+function cadastraVeiculo(e){
+    var modeloCarro = document.getElementById('modeloCarro').value;
+    var placaCarro = document.getElementById('placaCarro').value;
+    var qtd = document.getElementById('qtd').value;
+    var time = new Date();
+
+    if(!modeloCarro && !placaCarro){
+        alert("Por favor, preencha os campos em branco!");
+        return false;
+    }
+
+    carro = {
+        modelo: modeloCarro,
+        placa: placaCarro,
+        quant: qtd,
+        hora: time.getHours(),
+        minutos: time.getMinutes(),
+    }
+
+    
+   
+    console.log(carro);
+
+    if(localStorage.getItem('patio4') == null){
+        var carros = [];
+        carros.push(carro);
+        localStorage.setItem('patio4', JSON.stringify(carros));
+    }else{
+        var carros = JSON.parse(localStorage.getItem('patio4'));
+        carros.push(carro);
+        localStorage.setItem('patio4', JSON.stringify(carros));
+    }
+
+    document.getElementById('formulario').reset();
+
+    mostraPatio();
+    
+    e.preventDefault();
+}
+
+function apagarVeiculo(placa){
+    var carros = JSON.parse(localStorage.getItem('patio4'));
+
+    for(var i = 0; i < carros.length; i++){
+        if(carros[i].placa == placa){
+            carros.splice(i, 1);
+        }
+
+        localStorage.setItem('patio4', JSON.stringify(carros));
+    }
+
+    mostraPatio();
+}
+
+
+
+function mostraPatio(){
+    var carros = JSON.parse(localStorage.getItem('patio4'));
+    var carrosResultado = document.getElementById('resultados');
+
+    carrosResultado.innerHTML = '';
+
+    for(var i = 0; i < carros.length; i++){
+        var modelo = carros[i].modelo;
+        var placa = carros[i].placa;
+        var quant = carros[i].quant;
+        var minutos = carros[i].minutos;
+
+        carrosResultado.innerHTML += '<tr><td>' + modelo +
+         '</td><td>' + placa + '<td>' + quant +
+         '</td></td><td><button class="btn btn-danger" onclick="apagarVeiculo(\'' + placa +'\')">Excluir</button>' + '</tr>';
+    }
+
+
+
+}
